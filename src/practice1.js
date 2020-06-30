7c8d6e80dcab8229bf4f1acca17ef2699f4f58a1
linlifu2 <linlin.fu@thoughtworks.com>
2020-06-30 22:12:34 +0800

    finish the practice

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
-export { parseData };
+const parseData = (input) => {
+  const {data, column} = input;
+  let columnName = column.map(({name}) => name)
+  let result = []
+  data.map(d => {
+    result = [...result,
+      Object.assign(...columnName.map((k, i) => ({[k]: d[i]})))]
+  })
+  return result;
+}
+
+export {parseData};
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
-export { inject };
+const inject = (items, sections) => {
+  let temp = [...items].map(item => ([item]));
+  sections.forEach(({content, index}) => {
+    temp[index] = [content, ...temp[index]]
+  })
+  return temp.flat();
+}
+export {inject};
